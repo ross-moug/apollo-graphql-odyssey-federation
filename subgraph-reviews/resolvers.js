d1a52b7be2f7e6bb70a008c139706da022dd4de1
Ross Moug <ross.moug@gmail.com>
2022-04-09 16:28:40 +0100

    Extend Location in reviews subgraph

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -1,11 +1,15 @@
 const resolvers = {
   Query: {
-    latestReviews(_, __, {dataSources}) {
+    latestReviews(_, __, { dataSources }) {
       return dataSources.reviewsAPI.getLatestReviews();
     }
   },
+  Location: {
+    overallRating: ({ id }, _, { dataSources }) => dataSources.reviewsAPI.getOverallRatingForLocation(id),
+    reviews: ( { id }, _, { dataSources }) => dataSources.reviewsAPI.getReviewsForLocation(id)
+  },
   Mutation: {
-    submitReview(_, {review}, {dataSources}) {
+    submitReview(_, { review }, { dataSources }) {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
       return {code: 200, success: true, message: 'success', review: newReview};
     }
