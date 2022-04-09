6836d451843b0384be0914d04fd76d6277908692
Ross Moug <ross.moug@gmail.com>
2022-04-09 16:47:29 +0100

    Referencing an entity

--- subgraph-locations/resolvers.js
+++ subgraph-locations/resolvers.js
@@ -1,12 +1,11 @@
 const resolvers = {
   Query: {
-    locations(_, __, { dataSources }) {
-      return dataSources.locationsAPI.getAllLocations();
-    },
-    location(_, { id }, { dataSources }) {
-      return dataSources.locationsAPI.getLocation(id);
-    },
+    locations: (_, __, { dataSources }) => dataSources.locationsAPI.getAllLocations(),
+    location: (_, { id }, { dataSources }) => dataSources.locationsAPI.getLocation(id)
   },
+  Location: {
+    __resolveReference: ({ id }, { dataSources }) => dataSources.locationsAPI.getLocation(id)
+  }
 };
 
 module.exports = resolvers;
--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -1,15 +1,16 @@
 const resolvers = {
   Query: {
-    latestReviews(_, __, { dataSources }) {
-      return dataSources.reviewsAPI.getLatestReviews();
-    }
+    latestReviews: (_, __, { dataSources }) => dataSources.reviewsAPI.getLatestReviews()
   },
   Location: {
     overallRating: ({ id }, _, { dataSources }) => dataSources.reviewsAPI.getOverallRatingForLocation(id),
     reviews: ( { id }, _, { dataSources }) => dataSources.reviewsAPI.getReviewsForLocation(id)
   },
+  Review: {
+    location: ({ locationId }) => ({ id: locationId })
+  },
   Mutation: {
-    submitReview(_, { review }, { dataSources }) {
+    submitReview: (_, { review }, { dataSources }) => {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
       return {code: 200, success: true, message: 'success', review: newReview};
     }
